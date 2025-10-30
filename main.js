56a6793f9c2ba7a1b0312880727753b1034e5500
Robert@escola <robert.santos.almeida@escola.pr.gov.br>
2025-10-30 09:38:45 -0300

    h

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ avanca.forEach(button=>{
     const atual = document.querySelector('.ativo')
 const proximoPasso = "passo-" + this.getAttribute('data-proximo')
  atual. classList.remove('ativo')
-document.getElementById(proximoPasso).add('ativo')
+document.getElementById(proximoPasso).classList.add('ativo')
 })  
 }
 
