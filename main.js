f1557d097ebcf04c4a7e9c1546b6c760864590e8
Robert@escola <robert.santos.almeida@escola.pr.gov.br>
2025-10-30 09:26:43 -0300

    horas

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ console.log(avanca)
 
 avanca.forEach(button=>{
     button.addEventListener('click',function(){
-
+    const atual = document.querySelector('.ativo')
+const proximoPasso = "passo-" + this.getAttribute('data-proximo')
+ atual. classList. remove('ativo')
+document.getElementById(proximoPasso). addEventListener('ativo')
+})  
 }
 
-{
-
-})
+)
